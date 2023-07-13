1bd75a831a399dc48914abddab5171b20d2c8c30
SollyTlax <sollytlaks@gmail.com>
2023-07-13 11:58:56 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdhio.h>
+/**
+ * main - a program that print using a puts function
+ *
+ * Description:Use the function puts
+ * You are not allowed to use printf
+ * Your program should end with the value 0
+ * Return:0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
