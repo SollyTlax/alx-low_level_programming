4ca9dda4f4333b5738a731c2a21afe47cf35efbc
SollyTlax <sollytlaks@gmail.com>
2023-07-13 13:20:10 -0700

    UNIX is basically a simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - program that prints exactly 1 line and a new line
+ *
+ * Description: You are not allowed to use any functions
+ * listed in the NAME section of the man (3) printf or man
+ * (3) puts Your program should return 1
+ * Your program should compile without any warnings
+ * when using the -Wall gcc option
+ *
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
