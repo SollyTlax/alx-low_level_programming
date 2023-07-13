0dd505730d4d924e8fe09fbde2e3c41d63aa404d
SollyTlax <sollytlaks@gmail.com>
2023-07-13 12:04:49 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,6 @@
 /**
  * main - a program that print using a puts function
  *
- * Description:Use the function puts
- * You are not allowed to use printf
- * Your program should end with the value 0
  * Return:0 (Success)
  */
 int main(void)
