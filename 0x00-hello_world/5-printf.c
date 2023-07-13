22d24474f020f54f147211ec7ab825ed669a4497
SollyTlax <sollytlaks@gmail.com>
2023-07-13 12:28:26 -0700

    Print using print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a line using printf
+ *
+ * Description:You are not allowed to use the function puts
+ * Your program should return 0
+ * Your program should compile without warning when using the -Wall gcc option
+ *
+ * Return : 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
