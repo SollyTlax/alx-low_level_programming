1779a8d62020a91ecb80744536c1f6267e687213
SollyTlax <sollytlaks@gmail.com>
2023-07-15 22:18:14 -0700

    Numbers use putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -Write a program that prints all single digit numbers
+ * of base 10 starting from 0
+ * You are not allowed to use any variable of type char
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	for n;
+
+	for (n = 0; n < 10; n++)
+	{
+	putchar(n + '0');
+	}
+	putchar('\n');
+	return (0);
+
+}
