705fd672d2632df872385cd68a114a005e01210d
SollyTlax <sollytlaks@gmail.com>
2023-07-15 23:10:32 -0700

    combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Write a program that prints all possible
+ * combinations of single-digit numbers.
+ * Numbers must be separated by ,, followed by a space
+ * Numbers should be printed in ascending order
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+	putchar(n + '0');
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
