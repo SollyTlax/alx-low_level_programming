3258a880ddbc6282f748445010c5252e078a934f
SollyTlax <sollytlaks@gmail.com>
2023-07-31 12:51:10 -0700

    value of pointer to char

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -7,23 +7,5 @@
  */
 void set_string(char **s, char *to)
 {
-	free(*s);
-	if (to != NULL)
-	{
-	size_t len = 0;
-
-	while (to[len] != '\0')
-	{
-	len++;
-	}
-	*s = (char *)malloc((len + 1) * sizeof(char));
-	for (size_t i = 0; i <= len; i++)
-	{
-	(*s)[i] = to[i];
-	}
-	else
-	{
-	*s = NULL;
-	}
-	}
+	*s = t0;
 }
