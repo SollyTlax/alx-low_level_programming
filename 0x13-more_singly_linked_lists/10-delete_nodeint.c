928696e2dc80f1f014542444b3da279029c97e80
SollyTlax <sollytlaks@gmail.com>
2023-08-29 11:38:24 -0700

    delete node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *current, *next_node = *head;
-	unsigned int i = 0;
+	unsigned int i;
 
 	if (*current == NULL)
 		return (-1);
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(current);
 		return (1);
 	}
-	for (i = 0; current != NULL && i < index - 1; i++)
+	for (i = 0; i < (index - 1); i++)
 	{
 		if (current->next == NULL)
 			return (-1);
