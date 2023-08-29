f1551ddd952d4f5b66bd94e457ee7905402299f9
SollyTlax <sollytlaks@gmail.com>
2023-08-29 12:04:14 -0700

    reverse

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -7,19 +7,19 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL;
-	listint_t *next = NULL;
+	listint_t *prev_node = NULL;
+	listint_t *next_node = NULL;
 
 	while (*head != NULL)
 	{
-	next = (*head)->next;
-	(*head)->next = prev;
+	next_node = (*head)->next_node;
+	(*head)->next_node = prev_node;
 
-	prev = *head;
-	*head = next;
+	prev_node = *head;
+	*head = next_node;
 	}
 
-	*head = prev;
+	*head = prev_node;
 
 	return (*head);
 }
