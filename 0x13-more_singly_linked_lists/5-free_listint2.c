eefd178db9d3970a4c8b09096f4a1a115b9c3bc4
SollyTlax <sollytlaks@gmail.com>
2023-08-29 09:22:00 -0700

    get node

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,21 +2,24 @@
 /**
  * free_listint2 - Frees a listint_t list.
  * @head: Double Pointer to the head of the list.
+ *
+ * Description:sets the head to NULL.
  */
 void free_listint2(listint_t **head)
 {
 	if (head == NULL)
 		return;
 
-	listint_t *current = *head;
+	/*listint_t *current = *head;*/
 	listint_t *temp;
 
-	while (current != NULL)
+	while (*head)
 	{
-		temp = current;
-		current = current->next;
-		free(temp);
+		temp = (*head)->next;
+/*		current = current->next;*/
+		free(*head);
+		*head = temp;
 	}
 
-	*head = NULL;
+	head = NULL;
 }
