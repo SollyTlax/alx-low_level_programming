c2b5c119563a9347fa6ca0bc5ae7fd5d40944bd8
SollyTlax <sollytlaks@gmail.com>
2023-09-05 10:13:03 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,6 @@
 #include "main.h"
-
+#include <stdlib.h>
+ii
 /**
  * read_textfile - Reads a text file and prints it to standard output
  * @filename: The name of the file to read
@@ -13,17 +14,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t o_file, r_file, w_file;
 	char *buffer;
 
-	if (!filename || !letters)
+	if (filename == NULL)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
+	buffer = malloc(sizeof(char) * letters + 1);
 
-	if (!buffer)
+	if (buffer == NULL)
 		return (0);
 	0_file = open(filename, O_RDONLY);
 	r_file = read(o_file, buffer, letters);
 	w_file = write(STDOUT_FILENO, buffer, r_file);
 
-	if (o_file < 0 || r_file < 0 || w_file < 0)
+	if (o_file == -1)
 	{
 		free(buffer);
 		return (0);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,7 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unist.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
+int append_text_to_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
