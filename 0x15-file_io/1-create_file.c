c73a0175ec3074c63dc13d2b49e3c03c93dded66
SollyTlax <sollytlaks@gmail.com>
2023-09-05 10:35:27 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * create_file - Creates a file and writes text content to it
+ * @filename: The name of the file to create
+ * @text_content: The text content to write to the file
+ * (or NULL for an empty file)
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int o_file, w_file, len;
+
+	if (!filename)
+		return (-1);
+
+	o_file = open(filename, O_CREATE | O_RDWR | O_TRUNC, 0600);
+
+	if (o_file < 0)
+		return (-1);
+	while (text_content && text_content[len])
+		len++;
+
+	w_file = write(o_file, text_content, len);
+	if (w_file < 0)
+		return (-1);
+	close(o_file);
+	return (1);
+
+}
