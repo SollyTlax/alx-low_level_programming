2cbcbacfc2d8d8a9f7d86b65f9624b911cdfb049
SollyTlax <sollytlaks@gmail.com>
2023-09-08 10:21:15 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	o_file = open(filename, O_CREATE | O_RDWR | O_TRUNC, 0600);
+	o_file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (o_file < 0)
 		return (-1);
